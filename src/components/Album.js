e2ec35b8c55da2ad5d94c0d7e12e15188901cefa
Tanya Swift <tanyanicolerussi@gmail.com>
2018-11-19 14:32:45 -0700

    Assignment Bloc Jams React Player Bar Buttons completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -88,6 +88,14 @@ class Album extends Component {
     this.play();
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.min(currentIndex === 5, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
   render() {
     return(
       <section className="album">
@@ -122,6 +130,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     );
